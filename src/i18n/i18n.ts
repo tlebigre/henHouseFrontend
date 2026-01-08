18f24e9f48fe99fa12dc933ea8e03cf44225ad83
tlebigre <theo.lebigre@gmail.com>
2026-01-08 23:14:00 +0100

    i18n correction

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,9 +1,14 @@
-import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
+import { register, init, getLocaleFromNavigator, locale } from 'svelte-i18n';
 
 register('fr', () => import('./fr.json'));
 register('en', () => import('./en.json'));
 
 init({
 	fallbackLocale: 'fr',
-	initialLocale: getLocaleFromNavigator() ?? 'fr'
+	initialLocale: 'fr'
 });
+
+const navLocale = getLocaleFromNavigator();
+if (navLocale === 'en' || navLocale === 'fr') {
+	locale.set(navLocale);
+}
